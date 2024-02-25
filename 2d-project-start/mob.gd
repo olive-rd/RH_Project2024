7b9a5e8f73ccbcc28590841aaa94307561f570c5
Sir-Narly <158637648+Sir-Narly@users.noreply.github.com>
2024-02-25 04:24:42 -0600

    fixed minor bugs
    cam fix, body change, enemh moves

--- 2d-project-start/mob.gd
+++ 2d-project-start/mob.gd
@@ -1,11 +1,11 @@
 extends CharacterBody2D
 
-@onready var player = get_node("/root/Game/Player")
+@onready var player = get_node("/root/Game_scene/CharacterBody2D")
 
 func _ready():
 	$AnimatedSprite2D.play("chasing")
 
 func _physics_process(delta):
-	var direction = global_position.direction_to(player.global_postition)
+	var direction = global_position.direction_to(player.global_position)
 	velocity = direction * 75.0
 	move_and_slide()
